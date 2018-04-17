22882d56a6cd527f555921f617c58a28c80af6f0
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-17 23:53:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function theBeatlesPlay(mArray,iArray){
-  var mArray = []
-  var iArray = []
+  var combArray = []
+  for(var i=0; i< length.mArray; i++){
+    combArray[i] = `${mArray[i]} plays ${iArray[i]}`
+    
+  }
+  return combArray
 }
