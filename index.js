5870b08e49ed93cadd7159a3f339e85c9c9d971f
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-18 00:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ function johnLennonFacts(facts){
   }
   return facts
 }
+function iLoveTheBeatles(number){
+  var fArray = []
+  do{
+    fArray.push("I love the Beatles!")
+    number++
+  } while {number < 15}
+}
